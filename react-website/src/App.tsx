32a833bd85cdafb9838db77a42bcafc8829bfb05
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2024-11-13 01:13:42 +0400

    entire environment done

---
+++ react-website/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+const App = () => {
+  return (
+    <div>
+
+      <Box bg="white"
+      color="white"
+      px={4}
+      py={3}
+      boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1), 0px -10px 15px rgba(0, 0, 0, 0.15)"
+      position="relative">
+
+      </Box>
+      
+    </div>
+  )
+}
+
+export default App
