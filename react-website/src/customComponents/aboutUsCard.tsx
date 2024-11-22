fd3738942bd623cd1f063cc6a53dc7cc0d72382f
ZaidAljundiHW2 <za2038@hw.ac.uk>
2024-11-23 02:11:49 +0400

    updated responsiveness

--- react-website/src/customComponents/aboutUsCard.tsx
+++ react-website/src/customComponents/aboutUsCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Heading, Text, Flex, IconButton } from '@chakra-ui/react';
+import { Box, Heading, Text, Flex } from '@chakra-ui/react';
 import { CloseButton } from '@/components/ui/close-button';
 
 interface AboutUsCardProps {
@@ -16,7 +16,6 @@ const AboutUsCard: React.FC<AboutUsCardProps> = ({ onClose }) => {
       bg="black"
       color="white"
       width={['90%', '80%', '60%']} // Adjust width on different screen sizes
-      height={['122%', '112%', '92%']}
       padding={['30px', '40px', '60px']} // Adjust padding on different screen sizes
       borderRadius="8px"
       zIndex="1000"
@@ -45,36 +44,36 @@ const AboutUsCard: React.FC<AboutUsCardProps> = ({ onClose }) => {
 
       {/* Columns Container */}
       <Flex
-        justify="space-between"
-        mb={4}
-        gap={[4, 6]} // Adjust gap on different screen sizes
-        flexWrap="wrap"
-        textAlign="center"
+      justify="center"
+      mb={4}
+      gap={[4, 6]} // Adjust gap on different screen sizes
+      flexWrap="wrap"
+      textAlign="center"
       >
-        <Box flex="1" minW="250px" mb={[4, 0]}>
-          <Heading mb={6} fontSize={['3xl', '2xl', 'xl']}>Our Mission</Heading>
-          <Text fontSize={['xl', 'lg', 'md']} letterSpacing="wider">
-            Vektor Tech is a young and new institution that specialises in providing innovative solutions to various energy conservation related challenges. With a team composed of third year computer science students at Heriot-Watt University, the institute hopes to offer many services including a platform for smart home management and energy optimization in your very own home.
-          </Text>
-        </Box>
+      <Box flexBasis={['100%', '45%', '30%']} minW="250px" mb={[4, 0]}>
+        <Heading mb={6} fontSize={['xl', '2xl', '3xl']}>Our Mission</Heading>
+        <Text fontSize={['md', 'lg', 'xl']} letterSpacing="wider">
+          Vektor Tech is a young and new institution that specialises in providing innovative solutions to various energy conservation related challenges. With a team composed of third year computer science students at Heriot-Watt University, the institute hopes to offer many services including a platform for smart home management and energy optimization in your very own home.
+        </Text>
+      </Box>
 
-        <Box flex="1" minW="250px" mb={[4, 0]}>
-          <Heading mb={6} fontSize={['3xl', '2xl', 'xl']}>Our Values</Heading>
-          <Text fontSize={['xl', 'lg', 'md']} letterSpacing="wider">
-            At Vektor Tech, we believe that the rise of renewable energy is gaining momentum, and we would like to offer our services to make the best of what humans can do to better the environment. Our team works closely with clients to understand their specific needs and develop custom platforms like our debut project "Sukoon" to deliver measurable results.
-          </Text>
-        </Box>
+      <Box flexBasis={['100%', '45%', '30%']} minW="250px" mb={[4, 0]}>
+        <Heading mb={6} fontSize={['xl', '2xl', '3xl']}>Our Values</Heading>
+        <Text fontSize={['md', 'lg', 'xl']} letterSpacing="wider">
+          At Vektor Tech, we believe that the rise of renewable energy is gaining momentum, and we would like to offer our services to make the best of what humans can do to better the environment. Our team works closely with clients to understand their specific needs and develop custom platforms like our debut project "Sukoon" to deliver measurable results.
+        </Text>
+      </Box>
 
-        <Box flex="1" minW="250px" mb={[4, 0]}>
-          <Heading mb={6} fontSize={['3xl', '2xl', 'xl']}>Our Vision</Heading>
-          <Text fontSize={['xl', 'lg', 'md']} letterSpacing="wider">
-            Vektor Tech is committed to revolutionizing the way we live by integrating cutting-edge technology with energy-efficient solutions. Our vision is to provide smarter homes for a more sustainable future. We strive to empower individuals to reduce their environmental impact while enjoying the comfort of advanced automation and intelligent systems.
-          </Text>
-        </Box>
-      </Flex>
+      <Box flexBasis={['100%', '45%', '30%']} minW="250px" mb={[4, 0]}>
+        <Heading mb={6} fontSize={['xl', '2xl', '3xl']}>Our Vision</Heading>
+        <Text fontSize={['md', 'lg', 'xl']} letterSpacing="wider">
+          Vektor Tech is committed to revolutionizing the way we live by integrating cutting-edge technology with energy-efficient solutions. Our vision is to provide smarter homes for a more sustainable future. We strive to empower individuals to reduce their environmental impact while enjoying the comfort of advanced automation and intelligent systems.
+        </Text>
+      </Box>
+    </Flex>
 
       <Box justifyContent="center" alignContent="center" alignItems="center" textAlign="center">
-        <Text fontSize={['xl', 'lg', 'md']} letterSpacing="wider">
+        <Text fontSize={['md', 'lg', 'xl']} letterSpacing="wider">
           Our pursuit in mastering and applying the most functional and convenient energy management systems has led us to SUKOON as our main selling point. Our blend of strategic management and solution expertise with our small size has allowed us to create boutique and tailored services to our customers to achieve energy management.
         </Text>
       </Box>
