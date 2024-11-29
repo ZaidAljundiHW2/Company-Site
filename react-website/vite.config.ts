7fa17f5f96a5ae66c78350d38b3cb9fd3882480f
ZaidAljundiHW2 <za2038@hw.ac.uk>
2024-11-30 02:45:06 +0400

    updated deployment

--- react-website/vite.config.ts
+++ react-website/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Company-Site/react-website/',  // Important for GitHub Pages
+  base: '/Company-Site/',  // Important for GitHub Pages
   plugins: [react(), tsconfigPaths()],
 })
